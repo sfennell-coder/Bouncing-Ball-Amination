0ddc2467e9c445a4b2acb0e415175d10289b7c07
Scott Fennell <slfennell01@gmail.com>
2024-06-02 20:13:47 -0400

    Create Ball constructor with initial properties
    Create Ball constructor with initial properties

---
+++ script.js
@@ -0,0 +1,12 @@
+//U86364709
+
+//Create ball constructor with inital properties
+class Ball {
+    constructor(x, y, radius, dx, dy, color){
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.color = color;
+    };
+};
