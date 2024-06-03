e110527415dfafd07efd28ab32816e5fa0fc9eea
Scott Fennell <slfennell01@gmail.com>
2024-06-03 09:29:24 -0400

    Add update method with collision detection for Ball class.
    Add update method with collision detection for Ball class.

--- script.js
+++ script.js
@@ -1,20 +1,45 @@
 //U86364709
 
-//Create ball constructor with inital properties
+
+let canvas = document.querySelector("#animation");
+let ctx = canvas.getContext("2d");
+
+let width = canvas.width;
+let height = canvas.height;
+
+//Ball constructor with inital properties
 class Ball {
     constructor(x, y, radius, dx, dy, color){
         this.x = x;
         this.y = y;
+        this.radius = radius;
         this.dx = dx;
         this.dy = dy;
         this.color = color;
     }
+    // Draw method
     drawBall(ctx){
+        ctx.fillStyle = this.color;
         ctx.beginpath();
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
-        ctx.fillStyle = this.color;
         ctx.fill();
     }
-};
-let canvas = document.querySelector("#canvas");
-let ctx = canvas.getContext("2d");
+    // Update method for handling movement of ball
+    updateBall(canvas){
+        this.x += this.dx;
+        this.y += this.dy;
+
+        if(this.x + this.radius > canvas.width || this.x - this.radius  <= 0 ){
+            this.dx -= this.dx;
+        }
+        if(this.y + this.radius > canvas.height || this.y - this.radius <= 0){
+            this.dy -= this.dy;
+        }
+    }
+    draw(){
+        ctx.clearRect(0, 0, width, height);
+        this.drawBall();
+    }
+}
+
+
